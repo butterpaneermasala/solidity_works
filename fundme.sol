bc8114ec53188c43a3966dc35e81959d5358a362
butterpaneermasala <satyapradhan1732@gmail.com>
2024-11-05 00:41:13 +0530

    interface+solidity math

--- fundme.sol
+++ fundme.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 contract Fundme {
+    uint256 public minimumUSD = 5 * 1e18;
     function fund() public payable {
-        require(msg.value >= 1, "didn't send enough");
+        require(msg.value >= minimumUSD, "didn't send enough");
+    }
+
+    function GetPrice() public view returns(uint256) {
+        AggregatorV3Interface PriceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price, , ,) = PriceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function GetConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = GetPrice();
+        uint256 ethPriceinUSD = (ethPrice * ethAmount) / 1e18;
+        return ethPriceinUSD;
+    }
+
+    function GetVersion() public view returns(uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 }
